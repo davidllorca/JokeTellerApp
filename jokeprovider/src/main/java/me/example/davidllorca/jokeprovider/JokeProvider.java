2bc6f86c2b4313d621f569f3768c5d1e5c9b58e8
David Llorca <davidllorcabaron@gmail.com>
2018-06-25 20:06:39 +0200

    Initial commit

---
+++ jokeprovider/src/main/java/me/example/davidllorca/jokeprovider/JokeProvider.java
@@ -0,0 +1,9 @@
+package me.example.davidllorca.jokeprovider;
+
+/**
+ * Created by David Llorca <davidllorcabaron@gmail.com> on 25/6/18.
+ */
+
+public class JokeProvider {
+    public String getJoke() {return "this is a awesome joke";}
+}
